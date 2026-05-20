24a7d32837d70143562b739e7405bd84e10bcdf4
Ilya Kostikov <kostikoff613@gmail.com>
2026-05-20 22:53:03 +0300

    feat: add DB connection

---
+++ internal/storage/postgres/postgres.go
@@ -0,0 +1,28 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const pingTimeout = 5 * time.Second
+
+func New(ctx context.Context, dsn string) (*pgxpool.Pool, error) {
+	pool, err := pgxpool.New(ctx, dsn)
+	if err != nil {
+		return nil, fmt.Errorf("create postgres pool: %w", err)
+	}
+
+	pingCtx, cancel := context.WithTimeout(ctx, pingTimeout)
+	defer cancel()
+
+	if err := pool.Ping(pingCtx); err != nil {
+		pool.Close()
+		return nil, fmt.Errorf("ping postgres: %w", err)
+	}
+
+	return pool, nil
+}
