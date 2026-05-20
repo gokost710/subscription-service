a2e3e6e1ab3cdb490b38d182263a6f44c7783d70
Ilya Kostikov <kostikoff613@gmail.com>
2026-05-20 23:25:21 +0300

    chore: setup group routers

--- internal/http/router/router.go
+++ internal/http/router/router.go
@@ -3,9 +3,10 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/gokost710/subscription-service/internal/http/handler"
+	"github.com/gokost710/subscription-service/internal/service"
 )
 
-func New() *gin.Engine {
+func New(subscriptionService service.SubscriptionProvider) *gin.Engine {
 	gin.SetMode(gin.ReleaseMode)
 
 	r := gin.New()
@@ -13,6 +14,14 @@ func New() *gin.Engine {
 
 	healthHandler := handler.NewHealthHandler()
 	r.GET("/health", healthHandler.Check)
+
+	subscriptionHandler := handler.NewSubscriptionHandler(subscriptionService)
+
+	subscriptions := r.Group("/subscriptions")
+	{
+		subscriptions.POST("", subscriptionHandler.Create)
+		subscriptions.GET("/:id", subscriptionHandler.GetByID)
+	}
 
 	return r
 }
