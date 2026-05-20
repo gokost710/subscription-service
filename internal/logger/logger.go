ce3818ef4fc7cf007cafcce541347d6b908160e8
Ilya Kostikov <kostikoff613@gmail.com>
2026-05-20 22:14:46 +0300

    chore: add log level parsing

---
+++ internal/logger/logger.go
@@ -0,0 +1,31 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+	"strings"
+)
+
+func New(level string) *slog.Logger {
+	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: parseLevel(level),
+	})
+
+	log := slog.New(handler)
+	slog.SetDefault(log)
+
+	return log
+}
+
+func parseLevel(level string) slog.Level {
+	switch strings.ToLower(strings.TrimSpace(level)) {
+	case "debug":
+		return slog.LevelDebug
+	case "warn", "warning":
+		return slog.LevelWarn
+	case "error":
+		return slog.LevelError
+	default:
+		return slog.LevelInfo
+	}
+}
