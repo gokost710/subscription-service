ab83600e92359ce2e6aac6289dccfb3d8fbca9a6
Ilya Kostikov <kostikoff613@gmail.com>
2026-05-21 00:04:22 +0300

    docs: register swagger routes

--- internal/http/router/router.go
+++ internal/http/router/router.go
@@ -3,17 +3,22 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/gokost710/subscription-service/internal/http/handler"
+	"github.com/gokost710/subscription-service/internal/http/swagger"
 	"github.com/gokost710/subscription-service/internal/service"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func New(subscriptionService service.SubscriptionProvider) *gin.Engine {
 	gin.SetMode(gin.ReleaseMode)
+	swagger.Register()
 
 	r := gin.New()
 	r.Use(gin.Recovery())
 
 	healthHandler := handler.NewHealthHandler()
 	r.GET("/health", healthHandler.Check)
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	subscriptionHandler := handler.NewSubscriptionHandler(subscriptionService)
 
